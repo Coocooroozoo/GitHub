b039aaa67bd781d93f7cf6ac1a99a8124f6f15d5
Aleksandr Shchepelin <aleksandr.shchepelin@gmail.com>
2022-05-31 19:10:39 +0300

    aasd
    asddsa

---
+++ forgit2/forgit2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  forgit2
+//
+//  Created by Александр Андреевич Щепелин on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
